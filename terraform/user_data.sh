3cac4bafb8a9870038c7a6dc80cd159591d23556
Matthias Naber <naber.matthias@gmail.com>
2018-10-04 10:12:02 +0200

    removed docker build

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -58,6 +58,5 @@ usermod -aG docker ubuntu
 
 [[ -e /home/ubuntu/prediction-io ]] || git clone https://github.com/spring-media/rbbt-prediction-io.git /home/ubuntu/prediction-io
 chown -R ubuntu:ubuntu /home/ubuntu
-docker build -t pio /home/ubuntu/prediction-io/pio/
 eval $(aws ecr get-login --region eu-west-1 --no-include-email)
 docker-compose --file /home/ubuntu/prediction-io/docker-compose.prod.yml up --build
