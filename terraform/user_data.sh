5f59b8d170e9f15dc0e61a5da141f71b29018953
Matthias Naber <naber.matthias@gmail.com>
2018-10-09 11:29:36 +0200

    added docker compose

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -3,8 +3,12 @@ set -x
 
 yum --assumeyes install git docker
 service docker start
+curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+PATH=$PATH:/usr/local/bin
 
-[[ -e /home/ubuntu/prediction-io ]] || git clone https://github.com/spring-media/rbbt-prediction-io.git /home/ubuntu/prediction-io
-chown -R ubuntu:ubuntu /home/ubuntu
+usermod -aG docker ec2-user
+
+[[ -e ~/prediction-io ]] || git clone https://github.com/spring-media/rbbt-prediction-io.git ~/prediction-io
 eval $(aws ecr get-login --region eu-west-1 --no-include-email)
-docker-compose --file /home/ubuntu/prediction-io/docker-compose.prod.yml up --build
+docker-compose --file ~/prediction-io/docker-compose.prod.yml up --build
