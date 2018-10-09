03fc8b65ca56dabdeb75d3534fee78143531380a
Matthias Naber <naber.matthias@gmail.com>
2018-10-09 11:16:29 +0200

    install docker in user_data

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -x
 
-yum --assumeyes install git 
+yum --assumeyes install git docker
 
 [[ -e /home/ubuntu/prediction-io ]] || git clone https://github.com/spring-media/rbbt-prediction-io.git /home/ubuntu/prediction-io
 chown -R ubuntu:ubuntu /home/ubuntu
