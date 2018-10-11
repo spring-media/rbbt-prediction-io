47b2effb5804a5a47bc1165f12470c01dc9c708e
Sebastian Spoerer <sebastian.spoerer@weltn24.de>
2018-10-11 11:46:46 +0200

    to early chown

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -11,12 +11,13 @@ echo 'PATH=$PATH:/usr/local/bin' >> ~/.bashrc
 usermod -aG docker ec2-user
 
 
+[[ -e /opt/prediction-io ]] || git clone https://github.com/spring-media/rbbt-prediction-io.git /opt/prediction-io
+
 chown ec2-user:ec2-user /opt/prediction-io/scripts/cron.sh
 chmod +x /opt/prediction-io/scripts/cron.sh
 echo "* * * 2 * /opt/prediction-io/scripts/cron.sh" > mycron
 crontab -u ec2-user mycron
 rm -f mycron
 
-[[ -e /opt/prediction-io ]] || git clone https://github.com/spring-media/rbbt-prediction-io.git /opt/prediction-io
 eval $(aws ecr get-login --region eu-west-1 --no-include-email)
 docker-compose --file /opt/prediction-io/docker-compose.prod.yml up --build
