6e83077c002be924d9b15424c35874a83a877546
Sebastian Spoerer <sebastian.spoerer@weltn24.de>
2018-10-11 14:53:33 +0200

    : not needed

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -1,5 +1,5 @@
 eval $(aws ecr get-login --region eu-west-1 --no-include-email)
-docker run 933782373565.dkr.ecr.eu-west-1.amazonaws.com/pio:latest `date -d "2 hours ago" "+%Y-%m-%dT:%H:%M:%S"Z` `date "+%Y-%m-%dT:%H:%M:%S"Z`
+docker run 933782373565.dkr.ecr.eu-west-1.amazonaws.com/pio:latest `date -d "2 hours ago" "+%Y-%m-%dT%H:%M:%S"Z` `date "+%Y-%m-%dT%H:%M:%S"Z`
 # this is derived from the docker-compose env
 cat <<EOF > .env
 ES_HOST=vpc-prediction-io-mlilctsisxnuweu5pu3qd36vny.eu-west-1.es.amazonaws.com
