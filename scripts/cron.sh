4438c5ac74ae000586f192570aa9c1e8b8fab94c
Sebastian Spoerer <sebastian.spoerer@weltn24.de>
2018-10-11 13:57:32 +0200

    we need flexible time

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -1,5 +1,5 @@
 eval $(aws ecr get-login --region eu-west-1 --no-include-email)
-docker run --detach 933782373565.dkr.ecr.eu-west-1.amazonaws.com/pio:latest 2018-09-29T00:00:00Z 2018-10-09T00:00:00Z
+docker run --detach 933782373565.dkr.ecr.eu-west-1.amazonaws.com/pio:latest `date -d "2 hours ago" "+%Y-%m-%dT:%H:%M:%S"Z` `date "+%Y-%m-%dT:%H:%M:%S"Z`
 # this is derived from the docker-compose env
 cat <<EOF > .env
 ES_HOST=vpc-prediction-io-mlilctsisxnuweu5pu3qd36vny.eu-west-1.es.amazonaws.com
