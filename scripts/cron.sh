4108c2b8e878996553ebf829fdfa09bbc28c9089
Sebastian Spoerer <sebastian.spoerer@weltn24.de>
2018-10-16 14:43:36 +0200

    full path

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -4,7 +4,7 @@ lockfile -r 0 /tmp/pio_cron_sh.lock || exit 1
 
 eval $(aws ecr get-login --region eu-west-1 --no-include-email)
 docker pull 933782373565.dkr.ecr.eu-west-1.amazonaws.com/pio:importer-latest
-docker run 933782373565.dkr.ecr.eu-west-1.amazonaws.com/pio:importer-latest `date -d "2 hours ago" "+%Y-%m-%dT%H:%M:%S"Z` `date "+%Y-%m-%dT%H:%M:%S"Z`
+docker run --rm 933782373565.dkr.ecr.eu-west-1.amazonaws.com/pio:importer-latest `date -d "2 hours ago" "+%Y-%m-%dT%H:%M:%S"Z` `date "+%Y-%m-%dT%H:%M:%S"Z`
 # this is derived from the docker-compose env
 cat <<EOF > .env
 ES_HOST=vpc-prediction-io-mlilctsisxnuweu5pu3qd36vny.eu-west-1.es.amazonaws.com
@@ -15,8 +15,8 @@ HBASE_PORT=8085
 HDFS_HOST=pio-hbase.internal.welt.de
 HDFS_PORT=8020
 EOF
-docker run --env-file .env --dns 10.0.8.2 --env RUN_MODE=TRAIN_ONLY --rm 933782373565.dkr.ecr.eu-west-1.amazonaws.com/pio:ur-latest
+docker run --rm --env-file .env --dns 10.0.8.2 --env RUN_MODE=TRAIN_ONLY --rm 933782373565.dkr.ecr.eu-west-1.amazonaws.com/pio:ur-latest
 
-cd /opt/prediction-io/ && docker-compose restart 
+cd /opt/prediction-io/ && /usr/local/bin/docker-compose restart 
 
 rm -f /tmp/pio_cron_sh.lock
